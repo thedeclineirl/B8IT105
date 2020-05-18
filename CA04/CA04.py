aa9930b0e0d7fce6d18527b3c754b4dc4ea5444e
thedeclineirl <thomas.higgins.2@gmail.com>
2020-05-18 17:01:35 +0100

    CA04 - Dataset selected
    Sleceted a data off the London Bikes scheme from Kaggle

--- CA04/CA04.py
+++ CA04/CA04.py
@@ -13,18 +13,33 @@ Editted:        2020-05-11
 ##############################
 Brief
 Pandas Data Interpretation
-
+https://www.kaggle.com/hmavrodiev/london-bike-sharing-dataset/
 '''
 
 #Imports
 ##############################
 import numpy as np
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 ##############################
 
-dataset = pd.read_csv("E-Shop.csv")
+dataset = pd.read_csv("London_merged.csv")
 print(dataset.head())
 print(dataset.shape)
 print(dataset.info())
 print(dataset.describe())
+print(dataset.dtypes)
+
+# Plot
+# plt.figure(figsize=(8,8))
+# sns.heatmap(dataset,annot=True)
+# plt.title("London Bikes")
+# plt.xlabel("??")
+# plt.ylabel("??")
+# plt.show()
+
+
+
+
