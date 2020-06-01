03dd3d6d395966604298bc4c86400d1f4933e2d9
thedeclineirl <thomas.higgins.2@gmail.com>
2020-06-01 16:38:12 +0100

    CA05 submission
    CA05 submitted, all CAs completed, finally

--- CA05/CA05.R
+++ CA05/CA05.R
@@ -6,13 +6,12 @@
 # Assignment:     CA05
 # 
 # Created:        2020-05-11
-# Editted:        2020-05-31
+# Editted:        2020-06-01
 # ##################################################### #
 
 # install packages if not already installed
 if (!require('lubridate')) install.packages('lubridate')
 if (!require('ggplot2')) install.packages('ggplot2')
-
 
 # load libraries
 library(lubridate)
@@ -32,16 +31,18 @@ str(data)
 # check for missing data
 table(is.na(data))
 
-
+#examine the range of the cnt column 
+min(data$cnt)
+max(data$cnt)
 mean(data$cnt)
 sd(data$cnt)
 
-# data reorganising
 
+# data reorganisation
 
-
-# to keep the actual date I'm going to make the data into number
+# convert the date to a number
 data$Date <- as.numeric(substr(gsub("-", "", data$timestamp), 1, 9))
+
 # numeric categorical data for correlation
 data$Year <- lubridate::year(data$timestamp)
 data$Hour <- lubridate::hour(data$timestamp)
@@ -50,19 +51,18 @@ data$wDay <- lubridate::wday(data$timestamp)
 data$Month <- lubridate::month(data$timestamp)
 str(data)
 
-# check for errors before ddropping uneeded columns
-unique(data$Year)
-unique(data$Month)
-unique(data$Day)
-unique(data$wDay)
-unique(data$Hour)
+# check for errors/unexpected before dropping unneeded columns
+unique(data$Year)  # expected 2015:2017
+unique(data$Month) # expected 1:12
+unique(data$Day)   # expected 1:31
+unique(data$wDay)  # expected 1:7
+unique(data$Hour)  # expected 0:23
 
 # drop the original timestamp
 data$timestamp <- NULL
 str(data)
 
-
-# Plots using R standard library 
+# create plots 
 options(repr.plot.width=15, repr.plot.height=6)
 
 hist(data$cnt, col=3, main="Histogram of London Bike Rentals")
@@ -73,33 +73,4 @@ scatter.smooth(data$wDay,data$cnt, col=3, main="Bike Rentals by weekday")
 
 scatter.smooth(data$Year,data$cnt, col=12, main="Bike Rentals by Year")
 
-plot(data$wDay, data$cnt, col = rep(data$Year), pch = 19)
-legend("topleft", legend = unique(data$Year), col = unique(data$Year), pch = 19, bty = "n")
-
-
-# Plots using ggplot2
-
-ggplot(data,aes(x=cnt,fill=cnt)) + geom_area(fill="Red")
-
-ggplot(data)
-?ggplot2
-
-#To figure out if the temperature is in fahrenheit or celsius
-#I know it's London, they use celsius, but it never hurts to confirm
-ggplot(data, aes(x=data$Month, y=data$t1),col(3)) + geom_boxplot(fill="blue") + 
-  ggtitle("Temperature over Months") + ylab("Temperature (units ?)") + xlab("Month")
-
-# table of 
-table(data$Month, data$season)
-
-
-
-ggplot(data, aes(x=season, y=cnt)) + geom_area(fill="slateblue") + 
-  ggtitle("Temperature over Months") + ylab("Temperature (units ?)") + xlab("Month")
-
-boxplot(data$cnt, main="cnt", col="2")
-boxplot(data$t1, main="T1", col="3")
-boxplot(data$t2, main="T2", col="4")
-boxplot(data$t1,data$t2,main='T1 vs T2',col = 'orange')
-?boxplot
-        
+ggplot(data,aes(x=Month,y=cnt,fill=Year))+geom_col()+ggtitle("Bike rental by Month & Year")+xlab("Month")+ylab("count")
